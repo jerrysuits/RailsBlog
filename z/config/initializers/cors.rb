903b13ec64ca633a1d0af32d87f9fabba6a84242
k-koech <koechkelvin97@gmail.com>
2023-07-04 16:33:37 +0300

    sorted

--- z/config/initializers/cors.rb
+++ z/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['https://teeestreact.onrender.com', 'http://localhost:5174'] # Replace with your React frontend domain
+      origins ['https://teeestreact.onrender.com','http://localhost:5173', 'http://localhost:5174'] # Replace with your React frontend domain
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   end
