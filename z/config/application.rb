aa29e942a226363ef9f858da556c6279decdeca9
k-koech <koechkelvin97@gmail.com>
2023-07-04 19:12:00 +0300

    sorted

--- z/config/application.rb
+++ z/config/application.rb
@@ -10,6 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.session_store :cookie_store, key: '_your_app_session', domain: 'teest-asv8.onrender.com', secure: Rails.env.production?, same_site: :lax
 
     # Configuration for the application, engines, and railties goes here.
     #
--- z/config/initializers/session_store.rb
+++
@@ -1,2 +0,0 @@
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: 'teest-asv8.onrender.com'
-# , secure: Rails.env.production?, same_site: :lax
