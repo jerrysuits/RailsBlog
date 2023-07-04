7cc7a136d4f02f9b0b2d2e2392cd5c322031ddfd
k-koech <koechkelvin97@gmail.com>
2023-07-04 16:06:28 +0300

    deploy

---
+++ z/config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://teeestreact.onrender.com' # Replace with your React frontend domain
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+  end
+  
