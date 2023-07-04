a8f28d034380dc369318bb2a342b2833d29485e8
k-koech <koechkelvin97@gmail.com>
2023-07-04 13:25:28 +0300

    deploy

--- z/app/controllers/react_controller.rb
+++ z/app/controllers/react_controller.rb
@@ -2,6 +2,8 @@ class ReactController < ApplicationController
    skip_before_action :authorize, only: [:index]
 
    def index   
-      render file: 'public/index.html', layout: false
+      # render file: 'public/index.html', layout: false
+      render file: 'public/index.html'
+
    end
 end
