fcee07bac6c4c565b70dda8b0fb51783a3743889
k-koech <koechkelvin97@gmail.com>
2023-07-04 17:35:25 +0300

    sorted

--- z/config/initializers/session_store.rb
+++ z/config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: 'teest-asv8.onrender.com', secure: Rails.env.production?, same_site: :lax
+Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: 'teest-asv8.onrender.com'
+# , secure: Rails.env.production?, same_site: :lax
